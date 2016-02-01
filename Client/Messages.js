0e9836b870d24e2292ff6eed1d9e2d509d444fff
andrej <andrej351@yahoo.com>
2016-02-01 21:03:44 +1100

    Implement immediate adding of new messages.

--- Client/Messages.js
+++ Client/Messages.js
@@ -2,29 +2,45 @@
  * Initialises client side functionality for the messages view.
  */
 (function () {
+    // Get DOM references.
     var container = document.getElementById("container");
     var form = document.querySelector("form");
     var authorInput = form.querySelector("input[name=author]");
     var textInput = form.querySelector("input[name=text]");
+    // Init local store of messages.
     var messages = [];
+    // Set up event handler for adding new messages.
     form.onsubmit = onMessageSubmit;
+    // Get initial messages.
+    getMessages();
+    // Begin auto-refreshing messages.
+    setInterval(getMessages, 2000);
     /**
-     * Gets latest message data from the server and updates the view if new messages are present.
+     * Gets latest message data from the server and updates the view.
      */
-    function updateMessages() {
+    function getMessages() {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "messages/asJson");
         xhr.onload = function () {
-            var data = JSON.parse(xhr.responseText);
-            if (data.length > messages.length) {
-                var html = "";
-                data.map(function (message) {
-                    html += "\n<div>\n<h2>" + message.author + "</h2>\n<p>" + message.text + "</p>\n</div>";
-                });
-                container.innerHTML = html;
-            }
+            var newMessages = JSON.parse(xhr.responseText);
+            updateMessages(newMessages);
         };
         xhr.send();
+    }
+    /**
+     * Updates the view if new messages are present.
+     *
+     * @param newMessages The new list of messages.
+     */
+    function updateMessages(newMessages) {
+        if (newMessages.length > messages.length) {
+            messages = newMessages;
+            var html = "";
+            newMessages.map(function (message) {
+                html += "\n<div>\n<h2>" + message.author + "</h2>\n<p>" + message.text + "</p>\n</div>";
+            });
+            container.innerHTML = html;
+        }
     }
     /**
      * Event handles for submitting of new messages.
@@ -36,14 +52,12 @@
         var author = authorInput.value;
         var text = textInput.value;
         if (author && text) {
+            var newMessage = { author: author, text: text };
+            updateMessages(messages.concat([newMessage]));
             var xhr = new XMLHttpRequest();
             xhr.open("POST", "messages/create");
-            xhr.send(JSON.stringify({ author: author, text: text }));
+            xhr.send(JSON.stringify(newMessage));
         }
     }
-    // Get initial messages.
-    updateMessages();
-    // Begin auto-refreshing messages.
-    setInterval(updateMessages, 2000);
 })();
 //# sourceMappingURL=Messages.js.map
--- Client/Messages.ts
+++ Client/Messages.ts
@@ -18,37 +18,56 @@ interface Message {
  * Initialises client side functionality for the messages view.
  */
 (function () {
+    // Get DOM references.
     var container = document.getElementById("container");
     var form = <HTMLFormElement>document.querySelector("form");
     var authorInput = <HTMLInputElement>form.querySelector("input[name=author]");
     var textInput = <HTMLInputElement>form.querySelector("input[name=text]");
 
+    // Init local store of messages.
     var messages: Array<Message> = [];
 
+    // Set up event handler for adding new messages.
     form.onsubmit = onMessageSubmit;
 
+    // Get initial messages.
+    getMessages();
+
+    // Begin auto-refreshing messages.
+    setInterval(getMessages, 2000);
+
     /**
-     * Gets latest message data from the server and updates the view if new messages are present.
+     * Gets latest message data from the server and updates the view.
      */
-    function updateMessages() {
+    function getMessages() {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "messages/asJson");
-        xhr.onload = function () {
-            var data = <Array<Message>>JSON.parse(xhr.responseText);
-            if (data.length > messages.length) {
-                var html: string = "";
-                data.map((message) => {
-                    html += `
+        xhr.onload = () => {
+            var newMessages = <Array<Message>>JSON.parse(xhr.responseText);
+            updateMessages(newMessages);
+        };
+
+        xhr.send();
+    }
+
+    /**
+     * Updates the view if new messages are present.
+     *
+     * @param newMessages The new list of messages.
+     */
+    function updateMessages(newMessages: Array<Message>) {
+        if (newMessages.length > messages.length) {
+            messages = newMessages;
+            var html: string = "";
+            newMessages.map((message) => {
+                html += `
 <div>
 <h2>${message.author}</h2>
 <p>${message.text}</p>
 </div>`;
-                });
-                container.innerHTML = html;
-            }
-        };
-
-        xhr.send();
+            });
+            container.innerHTML = html;
+        }
     }
 
     /**
@@ -62,15 +81,11 @@ interface Message {
         var author = authorInput.value;
         var text = textInput.value;
         if (author && text) {
+            var newMessage = { author, text };
+            updateMessages(messages.concat([newMessage]));
             var xhr = new XMLHttpRequest();
             xhr.open("POST", "messages/create");
-            xhr.send(JSON.stringify({ author, text }));
+            xhr.send(JSON.stringify(newMessage));
         }
     }
-
-    // Get initial messages.
-    updateMessages();
-
-    // Begin auto-refreshing messages.
-    setInterval(updateMessages, 2000);
 })();
